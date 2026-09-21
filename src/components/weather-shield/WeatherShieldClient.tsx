12d21e8a45b371a83e9d4740557b6e28c1fb24e9
Risha-v <connectwithme.rishav@gmail.com>
2026-09-21 23:58:40 +0530

    chore: restore AppShell UI after successful architecture diagnostic

--- src/components/weather-shield/WeatherShieldClient.tsx
+++ src/components/weather-shield/WeatherShieldClient.tsx
@@ -2,14 +2,12 @@
 
 import React from 'react';
 import GoogleMapsProvider from '@/components/providers/GoogleMapsProvider';
-// import AppShell from './AppShell'; // Temporarily disabled for diagnostic
-import GooglePlacesDiagnostic from '@/components/search/GooglePlacesDiagnostic';
+import AppShell from './AppShell';
 
 export default function WeatherShieldClient() {
   return (
     <GoogleMapsProvider>
-      {/* <AppShell /> */}
-      <GooglePlacesDiagnostic />
+      <AppShell />
     </GoogleMapsProvider>
   );
 }
